e973c975d18abae2bcc2f5ffe5d2af52c81ce128
Amir Momenian <nainemom@gmail.com>
2022-06-23 20:37:34 +0430

    use normal fn instead of arrow fn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const fs = require('fs')
 const wordp = require(path.resolve(__dirname, './lib/wordp.js'))
 const dic = fs.readFileSync(path.resolve(__dirname, './dic'), 'utf8')
 
-module.exports = (word) => wordp.search(dic, word);
+module.exports = function(word) {
+    return wordp.search(dic, word)
+}
